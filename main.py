699f62838696e7552da1b0b8eb7ea6e149695950
Jonathan Lake-Thomas <jonathan.lakethomas@gmail.com>
2018-09-14 23:19:04 +0100

    Added time

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-import machine, ssd1306, time
+import machine, ssd1306, time, utime
+from ntptime import settime
 
 i2c = machine.I2C(scl=machine.Pin(16),sda=machine.Pin(5))
 oled = ssd1306.SSD1306_I2C(128,64,i2c,0x3c)
 
 counter = 0
 
-while counter < 50:
+while counter < 5:
     oled.fill(0)
     oled.text(str(counter),0,0)
+    oled.show()
+    time.sleep(1)
+    counter = counter + 1
+
+counter = 0
+
+while counter < 5:
+    settime()
+    tm = utime.localtime()
+    year, month, mday, hour, minute, second, weekday, yearday = tm
+    oled.fill(0)
+    oled.text(str(hour) + ":" + str(minute),0,0)
     oled.show()
     time.sleep(1)
     counter = counter + 1
