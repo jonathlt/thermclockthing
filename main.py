920e1299c39045357d9867325a450bf4a66dece9
Jonathan Lake-Thomas <jonathan.lakethomas@gmail.com>
2018-09-23 13:06:07 +0100

    Format temperature string

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ oled = ssd1306.SSD1306_I2C(128,64,i2c,0x3c)
 # Set up the temperature sensor
 dat = machine.Pin(2)
 ds = ds18x20.DS18X20(onewire.OneWire(dat))
-roms = ds.scan()
+
+def get_temp(ds_temp, num_dps):
+    roms = ds_temp.scan()
+    ds_temp.convert_temp()
+    time.sleep_ms(750)
+    for rom in roms:
+        temp = ds_temp.read_temp(rom)
+    return round(temp, num_dps)
 
 counter = 0
 
@@ -20,10 +27,7 @@ while counter < 5:
     oled.fill(0)
     oled.text(str(hour) + ":" + str(minute),0,0)
     #Show the temperature
-    ds.convert_temp()
-    time.sleep_ms(750)
-    for rom in roms:
-        temp = ds.read_temp(rom)
+    temp = get_temp(ds, 1)
     oled.text(str(temp),0,10)
     oled.show()
     time.sleep(1)
