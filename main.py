b77a0dcc9ed4cd13f2cff90e8dde38121e7e94ad
Jonathan Lake-Thomas <jonathan.lakethomas@gmail.com>
2018-09-23 18:47:29 +0100

    Added bst

--- main.py
+++ main.py
@@ -17,18 +17,71 @@ def get_temp(ds_temp, num_dps):
         temp = ds_temp.read_temp(rom)
     return round(temp, num_dps)
 
-counter = 0
+def dayofweek(d, m, y): 
+	t = [ 0, 3, 2, 5, 0, 3, 
+		5, 1, 4, 6, 2, 4 ] 
+	y -= m < 3
+	return (( y + int(y / 4) - int(y / 100) 
+			+ int(y / 400) + t[m - 1] + d) % 7) 
 
-while counter < 5:
-    #Show the time
+def is_bst(time):
+    # is it between November and Feb ?
+    month = time[1]
+    mday = time[2]
+    hour = time[3]
+    year = time[0]
+
+    def last_sunday_in_month(year, month):
+        last_sunday = 0
+        days_in_month = 31
+        for i in range(31, 0, -1):
+            if dayofweek(i, month, year) == 0:
+                last_sunday = i
+                break
+        return last_sunday
+
+    last_sunday_in_march = last_sunday_in_month(year, 3)
+    last_sunday_in_october = last_sunday_in_month(year, 10)
+
+    if month >= 11 or month <= 2:
+        return False
+    else:
+        if month >= 4 and month <= 9:
+            return True
+        if month == 3 and mday >= last_sunday_in_march and hour >= 1:
+            return True
+        if month == 10 and mday >= last_sunday_in_october and hour >= 2:
+            return True
+
+def get_time():
     settime()
     tm = utime.localtime()
     year, month, mday, hour, minute, second, weekday, yearday = tm
-    oled.fill(0)
-    oled.text(str(hour) + ":" + str(minute),0,0)
-    #Show the temperature
-    temp = get_temp(ds, 1)
-    oled.text(str(temp),0,10)
-    oled.show()
-    time.sleep(1)
-    counter = counter + 1
+    if is_bst(tm):
+        hour = hour + 1
+    return year, month, mday, hour, minute, second, weekday, yearday
+
+def format_time(hour, minute):
+    hour_str = ""
+    minute_str = ""
+    if hour < 10:
+        hour_str = "0" + str(hour)
+    else:
+        hour_str = str(hour)
+    if minute < 10:
+        minute_str = "0" + str(minute)
+    else:
+        minute_str = str(minute)
+    return hour_str + ":" + minute_str
+
+#Show the time
+tm = get_time()
+hour = tm[3]
+minute = tm[4]
+oled.fill(0)
+oled.text(format_time(hour, minute), 0, 0)
+#Show the temperature
+temp = get_temp(ds, 1)
+oled.text(str(temp),0,10)
+oled.show()
+
